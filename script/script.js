4983f2e6c9bf45416f70d94a44362a151ee7cdd4
Haley Cahoon <94021394+Colvig@users.noreply.github.com>
2023-04-26 21:45:05 -0400

    pokedex

--- script/script.js
+++ script/script.js
@@ -29,3 +29,52 @@ function removeActiveClassesSecond() {
     }) 
 }
 // end page 3
+
+
+
+
+// pokedex
+// tutorial here:
+// https://levelup.gitconnected.com/create-a-pokedex-with-vanilla-js-7aa580d6c96b
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 151;
+
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemons_number; i++) {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async id => {
+  const url =`https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
+}
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+  const { id, name, sprites, types } = pokemon;
+  const type = types[0].type.name;
+  const pokeInnerHTML = `
+  <div class="img-container">
+    <img src="${sprites.front_default}" alt="${name}" />
+  </div>
+  <div class="info">
+    <span class="number">${id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>
+  `;
+  pokemonEl.innerHTML = pokeInnerHTML;
+	poke_container.appendChild(pokemonEl);
+}
+
+fetchPokemons();
+
+
+
+function func() {
+	var type = str.toUpperCase();
+}
